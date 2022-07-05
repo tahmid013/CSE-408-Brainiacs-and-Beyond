e6411e4142242058a6dde6271b23f7170b8e8950
tahmid013 <37360863+tahmid013@users.noreply.github.com>
2022-07-06 03:20:21 +0600

    dev homepage react with routing

---
+++ development/my-ap/src/components/pages/DemoGuideVideo.js
@@ -0,0 +1,6 @@
+import React from "react";
+import '../../App.css';
+
+export default function DemoGuideVideo() {
+    return <h1>Demo Guide Tutorial Video</h1>
+}
---
+++ development/my-ap/src/components/pages/QuizMainPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import '../../App.css';
+
+export default function QuizMainPage() {
+    return <h1>Main Page</h1>
+}

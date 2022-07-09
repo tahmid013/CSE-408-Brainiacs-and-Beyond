29c2b3a8dd21ff0bfc6c35a4ac17faed7c251c58
ananan37326 <1705053@ugrad.cse.buet.ac.bd>
2022-07-09 15:35:33 +0600

    Backend initiated with django

---
+++ development/brainiaqs_backend/brainiaqs_backend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for brainiaqs_backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brainiaqs_backend.settings')
+
+application = get_wsgi_application()

afc74f748314d0d3bd1e6e7c26ad68af1c9b8ee4
tahmid013 <37360863+tahmid013@users.noreply.github.com>
2022-07-11 21:01:48 +0600

    node modules added

--- development/my-ap/src/components/pages/DemoGuideVideo.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import '../../App.css';
-
-export default function DemoGuideVideo() {
-    return <h1>Demo Guide Tutorial Video</h1>
-}
--- development/my-ap/src/components/pages/Sign-up.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import '../../App.css';
-
-export default function SignUp(){
-    return <h1>Sign Up</h1>
-}

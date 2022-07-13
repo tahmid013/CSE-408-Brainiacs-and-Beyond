c2ddb7c1d96ba1e1951861a8044e2fa2649af88a
tahmid013 <37360863+tahmid013@users.noreply.github.com>
2022-07-13 15:01:07 +0600

    frontend added 2

---
+++ development/react-frontend/src/App.js
@@ -0,0 +1,40 @@
+
+import React from 'react';
+import { BrowserRouter as Router ,Route , Routes } from 'react-router-dom';
+import './App.css';
+
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import SignUp from './components/pages/Sign-up';
+import QuizMainPage from './components/pages/QuizMainPage';
+import Features from './components/pages/Features';
+import HowWorks from './components/pages/HowWorks';
+import DemoGuideVideo from './components/pages/DemoGuideVideo';
+import Category from './components/pages/Category';
+const App = () =>  {
+    return (
+      <>
+        <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path = "/"  element = {<Home/>} />
+          <Route exact path = "/howitworks"  element = {<HowWorks/>} />
+          <Route exact path = "/features"  element = {<Features/>} />
+          <Route exact path = "/about"  element = {<About/>} />
+          <Route exact path = "/signup"  element = {<SignUp/>} />
+          <Route exact path = "/quizmainpage"  element = {<QuizMainPage/>} />
+          <Route exact path = "/demo"  element = {<DemoGuideVideo/>} />
+          <Route exact path = "/quizcategory"  element = {<Category/>} />
+        </Routes>
+       
+        </Router>
+
+        
+      </>
+    );
+  
+}
+export default App;
+
+
---
+++ development/react-frontend/src/components/pages/Sign-up.js
@@ -0,0 +1,6 @@
+import React from "react";
+import '../../App.css';
+
+export default function SignUp(){
+    return <h1>Sign Up</h1>
+}
